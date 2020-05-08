f6b13253639f9177e173d1edffd4f48ef52926fd
David <heysoydavid17>
2020-05-08 09:03:26 -0500

    hola

--- prueba/ViewController.swift
+++ prueba/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.setNavigationBarHidden(true, animated: true)
+        print("hola")
     }
 
 }
